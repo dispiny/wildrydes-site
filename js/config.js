d11e4d05f519d32fc41c5a9942b430579b2ec5c7
Jongmin Park <60399075+dispiny@users.noreply.github.com>
2022-03-17 20:45:30 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-northeast-2_02fVjbA7A', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3r654qeuf0jp4s7vnc0341pn89', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-northeast-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://a6v7xm3y78.execute-api.ap-northeast-2.amazonaws.com/test' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
